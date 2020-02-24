29d222894a9b339cfd72cdb1f905df61e755b577
Arka <arkaetos@gmail.com>
2020-02-24 22:50:35 +0100

    changement api pour le multi (photon -> Forge Network) + rigolade

---
+++ The_Project_B/Assets/Sandbox/Alexandre_(Arka)/Script/MonPetitCube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class MonPetitCube : BasicCubeBehavior
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(networkObject == null)
+        {
+            return;
+        }
+
+
+        if (!networkObject.IsOwner)
+        {
+            transform.position = networkObject.position;
+            transform.rotation = networkObject.rotation;
+
+            return;
+        }
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * 5f * Time.deltaTime;
+
+        networkObject.position = transform.position;
+        networkObject.rotation = transform.rotation;
+
+    }
+}
